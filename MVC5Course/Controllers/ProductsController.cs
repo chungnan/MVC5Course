862767d9d57bf51493fbf18c51706df00b513dbe
Chung-Nan Lin <lin.jacky@gmail.com>
2018-07-07 12:10:16 +0800

    07 練習透過 ViewModel 建立表單頁面與透過 ViewModel 接資料

--- MVC5Course/Controllers/ProductsController.cs
+++ MVC5Course/Controllers/ProductsController.cs
@@ -42,6 +42,24 @@ namespace MVC5Course.Controllers
             return View(data);
         }
 
+        public ActionResult AddNewProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddNewProduct(ProductViewModel data)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            // TODO 新增 Product 資料
+
+            return RedirectToAction("Index2");
+        }
+
         // GET: Products/Details/5
         public ActionResult Details(int? id)
         {
