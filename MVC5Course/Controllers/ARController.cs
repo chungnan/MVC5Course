1fde1e0df116bba1a4881afa17ecc88f32482cf8
Chung-Nan Lin <lin.jacky@gmail.com>
2018-07-08 16:13:23 +0800

    19 練習移除 WebFormViewEngine 以加速 View 頁面被找到的時間

---
+++ MVC5Course/Controllers/ARController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    // 練習使用各種 Active Result 方式
+    public class ARController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ViewTest()
+        {
+            return View();
+        }
+    }
+}
