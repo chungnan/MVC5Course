951de9c3dc3585a188450f1fbf702b6d598d32b9
Chung-Nan Lin <lin.jacky@gmail.com>
2018-07-14 11:09:31 +0800

    25 練習 ViewData, ViewBag, TempData 的各種用法

--- MVC5Course/Controllers/MBController.cs
+++ MVC5Course/Controllers/MBController.cs
@@ -13,5 +13,45 @@ namespace MVC5Course.Controllers
         {
             return View();
         }
+
+        // 示範 ViewData.Model
+        public ActionResult ViewDataModelDemo()
+        {
+            var data = "這是 ViewData.Model Demo";
+            ViewData.Model = data;
+            return View();
+        }
+
+        // 示範 ViewBag.KeyName
+        // 骨子裡用的是 ViewData
+        public ActionResult ViewBagDemo()
+        {
+            ViewBag.Text = "這是 ViewBag Demo";
+            return View();
+        }
+
+        // 示範 ViewData["KeyName"]
+        public ActionResult ViewDataDemo()
+        {
+            ViewData["Text"] = @"這是 ViewData[""Text""] Demo";
+            return View();
+        }
+
+        // 示範 TempData ["KeyName"]
+        /* TempData 使用情境
+         * 當表單資料送出後，先透過 TempDataSave 取得輸入的資料
+         * 中間需要處理輸入的資料儲存到 DB
+         * 接著在跳轉到完成頁面
+        */
+        public ActionResult TempDataSave()
+        {
+            TempData["Text"] = @"這是 TempData[""Text""] Demo";
+            return RedirectToAction("TempDataDemo");
+        }
+
+        public ActionResult TempDataDemo()
+        {
+            return View();
+        }
     }
 }
